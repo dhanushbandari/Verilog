49fb0e79917f6d992a2cb5afe4067d49d3740bef
dhanshbandari <dhanushsn17@gmail.com>
2026-08-02 14:30:24 +0530

    New Update

--- Gatelevel_Modeling/Half_adder/Half_adder.srcs/sources_1/new/design.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-// Half adder using Gate Level Abstaraction
-module halfadder(
-input a,b, // Input Ports 
-output sum,carry); // Output Ports
-xor (sum,a,b); // Xor operation for Sum A^B
-and(carry,a,b); // And operation to Carry A&B
-endmodule
